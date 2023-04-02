8975b98551ae280ce80e21b3b43a010eed57c51b
Salex <123@13.com>
2023-04-02 14:25:38 +0300

    Init commit

---
+++ src/test/java/com/example/smppdemo/SmppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smppdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
